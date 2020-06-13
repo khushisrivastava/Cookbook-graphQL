ab6d4d3668555f24fa5350f9935e0166a3faef7f
Khushi <khushisrivastava53@gmail.com>
2020-06-14 00:51:05 +0530

    Created an endponit that works with relay

--- cookbookGraphene/urls.py
+++ cookbookGraphene/urls.py
@@ -6,5 +6,5 @@ from cookbookGraphene.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
